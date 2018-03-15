df0392d9892eb755be589df8bd16aa5f35f77117
Anthony Chu <antchu@microsoft.com>
2018-03-15 15:40:18 -0700

    Add show existing drawing on load

--- SignalRDraw/Hubs/DrawHub.cs
+++ SignalRDraw/Hubs/DrawHub.cs
@@ -1,3 +1,6 @@
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.SignalR;
 
@@ -5,14 +8,31 @@ namespace SignalRDraw
 {
     public class DrawHub : Hub
     {
+        private static ConcurrentBag<Stroke> strokes = new ConcurrentBag<Stroke>();
         public async Task NewStroke(Point start, Point end, string color)
         {
-            await Clients.Others.SendAsync("newStroke", start, end, color);
+            var task = Clients.Others.SendAsync("newStroke", start, end, color);
+            strokes.Add(new Stroke
+            {
+                Start = start,
+                End = end,
+                Color = color
+            });
+            await task;
         }
 
         public async Task ClearCanvas()
         {
-            await Clients.Others.SendAsync("clearCanvas");
+            var task = Clients.Others.SendAsync("clearCanvas");
+            strokes.Clear();
+            await task;
+        }
+
+        public override async Task OnConnectedAsync()
+        {
+            await Clients.Caller.SendAsync("clearCanvas");
+            var tasks = strokes.Select(s => Clients.Caller.SendAsync("newStroke", s.Start, s.End, s.Color));
+            await Task.WhenAll(tasks);
         }
     }
 }
---
+++ SignalRDraw/Hubs/Stroke.cs
@@ -0,0 +1,9 @@
+namespace SignalRDraw
+{
+    public class Stroke
+    {
+        public Point Start { get; set; }
+        public Point End { get; set; }
+        public string Color { get; set; }
+    }
+}
